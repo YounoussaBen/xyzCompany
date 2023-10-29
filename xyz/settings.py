7d50f93dd02f442536bff334b6d2fcaca8dcd9e1
geazyskay <younoussaabdourhaman@gmail.com>
2023-10-29 14:36:45 +0000

    settings

--- xyz/settings.py
+++ xyz/settings.py
@@ -128,7 +128,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Define the root directory where your static files are stored
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
